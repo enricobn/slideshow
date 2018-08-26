a4ceb60735774867a7661be0a9fa47b9a0bd0619
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-08-26 07:53:59 +0200

    Add bouncing enemies.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 extern crate ggez;
+extern crate rand;
 use ggez::*;
 use ggez::conf::FullscreenType;
 use ggez::event::{self, EventHandler, Keycode, Mod};
 use ggez::graphics::{DrawMode,Point2,Rect,Color};
 use std::ops::IndexMut;
+use rand::*;
+use std::time::Instant;
 
 fn main() {
     let mut c = conf::Conf::new();
     c.window_setup = c.window_setup.title("Quad fight");
-    c.window_mode.fullscreen_type = FullscreenType::Desktop;
+    // c.window_mode.fullscreen_type = FullscreenType::Desktop;
     // c.window_mode.width = 60;
     // c.window_mode.height = 60;
     c.window_mode.vsync = true;
@@ -19,10 +22,7 @@ fn main() {
         println!("{}x{}", width, height);
     }
 
-    let mut enemies = Vec::new();
-    enemies.push(Enemy::new(700.0, 10.0, 100.0, 100.0, Color::new(1.0, 0.0, 0.0, 1.0), -10.0, 0.0));
-
-    let mut state = MainState{player: Player::new(), enemies};
+    let mut state = MainState::new();
 
     event::run(ctx, &mut state).unwrap();
 }
@@ -46,6 +46,16 @@ impl Enemy {
     fn update(&mut self) {
         self.x += self.vx;
         self.y += self.vy;
+
+        if self.y < 0.0 {
+            self.y = -self.y;
+            self.vy = -self.vy;
+        }
+
+        if self.y + self.height > 600.0 {
+            self.y = 600.0 - self.height;//self.y - (self.y + self.height - 600.0);
+            self.vy = -self.vy;
+        }
     }
 
 }
@@ -69,17 +79,49 @@ impl Player {
 struct MainState {
     player: Player,
     enemies: Vec<Enemy>,
+    last_enemy_added: Instant,
+}
+
+impl MainState {
+
+    fn new() -> MainState {
+        MainState{player: Player::new(), enemies: Vec::new(), last_enemy_added: Instant::now()}
+    }
+
+    fn last_enemy_added_millis(&self) -> u64 {
+        let duration = self.last_enemy_added.elapsed();
+
+        duration.as_secs() * 1_000 + duration.subsec_millis() as u64
+    }
+
 }
 
 impl event::EventHandler for MainState {
 
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        let mut rng = thread_rng();
+
+        if self.enemies.len() < 10 &&  self.last_enemy_added_millis() > 300 {
+            let vy = rng.gen_range(-10.0, 10.0);
+            self.enemies.push(Enemy::new(800.0, rng.gen_range(0.0, 500.0), 100.0, 100.0, Color::new(1.0, 0.0, 0.0, 1.0), -10.0, vy));
+            self.last_enemy_added = Instant::now();
+        }
+
+        let mut to_remove = Vec::new();
         let mut i: usize = 0;
         while i < self.enemies.len() {
             let enemy = self.enemies.index_mut(i);
             enemy.update();
+            if enemy.x + enemy.width < 0.0 {
+                to_remove.push(i);
+            }
             i += 1;
         }
+
+        for i in to_remove {
+            self.enemies.remove(i);
+        }
+
         Ok(())
     }
 
@@ -115,4 +157,5 @@ impl event::EventHandler for MainState {
             _ => (), // Do nothing
         }
     }
+
 }
